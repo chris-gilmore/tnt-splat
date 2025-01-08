66fd9b308d6914db6f60cc21493d3729e51b7f76
Your Name <you@example.com>
2025-01-07 23:41:27 -0600

    UnkStruct_5, linescan, lineeffect

--- include/newtetris/lineeffect.h
+++ include/newtetris/lineeffect.h
@@ -2,7 +2,8 @@
 #define _LINEEFFECT_H_
 
 typedef struct {
-  /* 0x0   */ u8   pad0[0x30];
+  /* 0x0  */ u8   pad0[0x2E]; // pad0[0x30];
+  /* 0x2E */ u16  unk2E;
 } LineEffect; // 0x30 bytes
 
 extern u8   LineEffect_Update(LineEffect *, u8);
