bedf65beb7c986cf03500b71f434f10354ea346b
Chris Gilmore <chris.gilmore@gmail.com>
2025-02-02 12:27:47 -0600

    not much

--- include/newtetris/0497E0.h
+++ include/newtetris/0497E0.h
@@ -1,7 +1,7 @@
 #ifndef _0497E0_H_
 #define _0497E0_H_
 
-extern void   debug_print_reason_routine(u8 *, u8 *);
+extern void   debug_print_reason_routine(const char *, const char *);
 extern void   func_80084630(void (*)(void), u16 *, u16, u16);
 
 #endif /* !_0497E0_H_ */
