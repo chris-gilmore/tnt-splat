aa9f259c9adcca06d6d918d7f55530f1f18727fc
Chris Gilmore <chris.gilmore@gmail.com>
2026-04-05 03:18:00 -0500

    nothing special

--- src/newtetris/069170.c
+++ src/newtetris/069170.c
@@ -6,10 +6,22 @@
   being highly unlikely to match the handwritten assembly.
 */
 
+/*
+  cvt.w.s    $ft0, $fa0
+  mfc1       $v0, $ft0
+  jr         $ra
+   nop
+*/
+
+/*
+  cvt.w.s converts float to integer
+  rounding mode on the N64 uses FPCSR_RM_RN (round to nearest)
+*/
+
 #pragma GLOBAL_ASM("asm/nonmatchings/newtetris/069170/func_800A2EF0.s")
 /*
 s32 func_800A2EF0(f32 arg0) {
-  return arg0;
+  return lroundf(arg0);
 }
 */
 
